5781b0acd7088c358dfa5cbf8f240640ba9afbb2
Danish Rashidin <danishrashidin@gmail.com>
2020-06-25 10:24:57 +0800

    updates

--- activities.php
+++ activities.php
@@ -2,7 +2,7 @@
 include_once "config.php";
 
 if (isset($_SESSION['Student_ID'])) {
-    $student_id = $_SESSION['Student_ID'];
+  $student_id = $_SESSION['Student_ID'];
 }
 ?>
 
@@ -12,109 +12,109 @@ if (isset($_SESSION['Student_ID'])) {
 <header id="header">
   <h1 class="title-header text-center">All College Activities</h1>
 </header>
+<div class="activities">
+  <div class="card-container">
+    <div id="wrapper">
+      <?php
+      $sql = "SELECT * FROM Activity";
+      $result = $connection->query($sql);
+      echo '<div class="card-row">';
+      while ($a = $result->fetch_array()) {
+        $activity_id = $a['Activity_ID'];
+        $activity_no = substr($activity_id, 0, 1);
+        $activity_name = $a['Activity_Name'];
+        $college = $a['College_ID'];
+        $reg_date = $a['Reg_Dead'];
 
-<div class="card-container">
-  <div id="wrapper">
-    <?php
-$sql = "SELECT * FROM Activity";
-$result = $connection->query($sql);
-echo '<div class="card-row">';
-while ($a = $result->fetch_array()) {
-    $activity_id = $a['Activity_ID'];
-    $activity_no = substr($activity_id, 0, 1);
-    $activity_name = $a['Activity_Name'];
-    $college = $a['College_ID'];
-    $reg_date = $a['Reg_Dead'];
+        $rem = strtotime($reg_date . ' 00:00:00') - time();
+        $day = floor($rem / 86400);
+        $hr = floor(($rem % 86400) / 3600);
+        $min = floor(($rem % 3600) / 60);
+        $sec = ($rem % 60);
 
-    $rem = strtotime($reg_date . ' 00:00:00') - time();
-    $day = floor($rem / 86400);
-    $hr = floor(($rem % 86400) / 3600);
-    $min = floor(($rem % 3600) / 60);
-    $sec = ($rem % 60);
+        if (isset($student_id)) {
+          $registrationQuery = "SELECT * FROM registration WHERE Student_ID= '$student_id' AND Activity_ID = '$activity_id'";
 
-    if (isset($student_id)) {
-        $registrationQuery = "SELECT * FROM registration WHERE Student_ID= '$student_id' AND Activity_ID = '$activity_id'";
+          $registrationResult = $connection->query($registrationQuery);
+          $registration = $registrationResult->fetch_array();
 
-        $registrationResult = $connection->query($registrationQuery);
-        $registration = $registrationResult->fetch_array();
+      ?>
+          <div class="card-<?php echo $activity_no; ?> cards fancycard" style="margin: auto;">
+            <div class="wrapper">
+              <div class="header">
+                <div class="countdown">
+                  <ul>
+                    <li><span class="days"> <?php echo $day ?> </span>Days</li>
+                    <li><span class="hours"> <?php echo $hr ?> </span>Hours</li>
+                    <li><span class="minutes"> <?php echo $min ?> </span>Minutes</li>
+                  </ul>
+                </div>
+                <div class="menu-content">
+                  <span class="college --noDisplay" style="display:none"><?php echo $college; ?></span>
+                  <span class="college">KK<?php echo $college; ?></span>
+                </div>
+                <div class="menu-status">
+                  <span class="status">
+                    <?php
+                    if (isset($registration)) {
+                      echo $registration['Status'];
+                    } else {
+                      echo "Available";
+                    }
+                    ?></span>
+                </div>
+              </div>
 
-        ?>
-      <div class="card-<?php echo $activity_no; ?> cards fancycard" style="margin: auto;">
-        <div class="wrapper">
-          <div class="header">
-            <div class="countdown">
-              <ul>
-                <li><span class="days"> <?php echo $day ?> </span>Days</li>
-                <li><span class="hours"> <?php echo $hr ?> </span>Hours</li>
-                <li><span class="minutes"> <?php echo $min ?> </span>Minutes</li>
-              </ul>
+              <div class="data">
+                <div class="content">
+                  <h1 class="title">
+                    <a><?php echo $activity_name; ?></a><input type="hidden" name="activity_name" class="activity_name" value="<?php echo $activity_name; ?>"></input>
+                  </h1>
+                  <?php
+                  if (isset($registration)) {
+                    echo '<a  class="button --cancelRegister">Cancel Registration</a>';
+                  } else {
+                    echo '<a  class="button --register">Register Now!</a>';
+                  }
+                  ?>
+                </div>
+              </div>
             </div>
-            <div class="menu-content">
-              <span class="college --noDisplay" style="display:none"><?php echo $college; ?></span>
-              <span class="college">KK<?php echo $college; ?></span>
-            </div>
-            <div class="menu-status">
-              <span class="status">
-                <?php
-if (isset($registration)) {
-            echo $registration['Status'];
+          </div>
+        <?php
         } else {
-            echo "Available";
-        }
-        ?></span>
-            </div>
-          </div>
-
-          <div class="data">
-            <div class="content">
-              <h1 class="title">
-                <a ><?php echo $activity_name; ?></a><input type="hidden" name="activity_name" class="activity_name" value="<?php echo $activity_name; ?>"></input>
-              </h1>
-              <?php
-if (isset($registration)) {
-            echo '<a  class="button --cancelRegister">Cancel Registration</a>';
-        } else {
-            echo '<a  class="button --register">Register Now!</a>';
-        }
         ?>
-            </div>
-          </div>
-        </div>
-      </div>
-    <?php
-} else {
-        ?>
-  <div class="card-<?php echo $activity_no; ?> cards fancycard" style="margin: auto;">
-        <div class="wrapper">
-          <div class="header">
-            <div class="countdown">
-              <ul>
-                <li><span class="days"> <?php echo $day ?> </span>Days</li>
-                <li><span class="hours"> <?php echo $hr ?> </span>Hours</li>
-                <li><span class="minutes"> <?php echo $min ?> </span>Minutes</li>
-              </ul>
-            </div>
-            <div class="menu-content">
-              <span class="college --noDisplay" style="display:none"><?php echo $college; ?></span>
-              <span class="college">KK<?php echo $college; ?></span>
+          <div class="card-<?php echo $activity_no; ?> cards fancycard" style="margin: auto;">
+            <div class="wrapper">
+              <div class="header">
+                <div class="countdown">
+                  <ul>
+                    <li><span class="days"> <?php echo $day ?> </span>Days</li>
+                    <li><span class="hours"> <?php echo $hr ?> </span>Hours</li>
+                    <li><span class="minutes"> <?php echo $min ?> </span>Minutes</li>
+                  </ul>
+                </div>
+                <div class="menu-content">
+                  <span class="college --noDisplay" style="display:none"><?php echo $college; ?></span>
+                  <span class="college">KK<?php echo $college; ?></span>
+                </div>
+              </div>
+
+              <div class="data">
+                <div class="content">
+                  <h1 class="title">
+                    <a><?php echo $activity_name; ?></a><input type="hidden" name="activity_name" class="activity_name" value="<?php echo $activity_name; ?>"></input>
+                  </h1>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div class="data">
-            <div class="content">
-              <h1 class="title">
-                <a ><?php echo $activity_name; ?></a><input type="hidden" name="activity_name" class="activity_name" value="<?php echo $activity_name; ?>"></input>
-              </h1>
-            </div>
-          </div>
-        </div>
-      </div>
-
-<?php
-}
-}
-?>
-  </div>
+      <?php
+        }
+      }
+      ?>
+    </div>
   </div>
 
   <div class="activityModal --registration">
@@ -183,8 +183,6 @@ if (isset($registration)) {
       </fieldset>
     </form>
   </div>
-
-
 </div>
 <script src="js/activity.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js " defer></script>
